f04d869e1b18123ee7c5b8c701f28ec429ceea99
Raveendra Lingareddygari <raveendra.lingareddygari@outlook.com>
2019-01-05 23:07:29 +0530

    Module 2 commit.

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -43,10 +43,13 @@ public class CartController extends HttpServlet {
 		try {
 			switch(action) {
 				case "/addcart":
-					 addToCart(request, response);
-           break;
-        default:
-           break;
+					addToCart(request, response);
+					break;
+				case "/delete":
+					deleteFromCart(request, response);
+					break;
+		        default:
+		        	break;
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -85,5 +88,11 @@ public class CartController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+	}
+	private void deleteFromCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int index = Integer.parseInt(request.getParameter("index"));
+		ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+		cart.deleteCartItem(index);
 	}
 }
